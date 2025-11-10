b2d0a188d10c10b1e643626d09840b616d651da5
P M Sai Akassh <akasshpm@gmail.com>
2025-11-10 23:38:15 +0530

    Delete b.js

--- b.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-    let scroll = scrollY;
-    const section = document.querySelector("section");
-    const overlay = document.querySelector(".overlay");
-
-    section.style.backgroundSize = `${100 + (scroll * 0.1)}%`;
-    overlay.style.background = `rgba(0, 0, 0, ${scroll * 1.7 /
-        window.innerHeight})`
-})
